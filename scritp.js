7402f4447c9efdec839d444856803e82543fd581
vikaspal9131 <vikaspal968562@gmail.com>
2024-05-21 23:19:21 +0530

    upload folder

---
+++ scritp.js
@@ -0,0 +1,28 @@
+// gsap.to(".box",{
+//   x:1000,
+//   duration :10,
+//   delay:1,
+//   backgroundColor: "white",
+//   borderRadius: "50%"
+// })
+// // gsap.from(".box",{
+// //   x:1000,
+// //   duration :10,
+// //   delay:1
+// // // })
+// // gsap.to(".box2",{
+// //   y:1000,
+// //   duration :10,
+// //   delay:1
+// // })
+
+
+gsap.from("h1",{
+  color:"red",
+  duration :2,
+  delay:1,
+  stagger:-1,
+  opacity:0,
+})
+
+
