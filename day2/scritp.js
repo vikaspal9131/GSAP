4a2ffe7af02ebb15c9272dc97cdf926e9be2aa96
vikaspal9131 <vikaspal968562@gmail.com>
2024-05-22 22:38:01 +0530

    learn scroll trigger

---
+++ day2/scritp.js
@@ -0,0 +1,19 @@
+gsap.from(".page1 .box",{
+  scale:0,
+  delay:1,
+  duration:3,
+  rotate:360,
+}) 
+gsap.from(".page2 .box",{
+  scale:0,
+  delay:1,
+  duration:2,
+  rotate:360,
+
+  scrollTrigger:{
+    trigger:".page2 .box",
+    scroller:"body",
+    markers:true,
+    start:"top 60%",
+  }
+}) 
